0d2a9fbfcf0385091424a2dc55397f1eb9cfca27
Dmytro Yaremyshyn <dmytroy@dxspark.com>
2024-06-29 02:35:21 +0100

    Code refactor

--- LetsGetWeather/LetsGetWeather/UI/Controllers/WeatherViewController.swift
+++ LetsGetWeather/LetsGetWeather/UI/Controllers/WeatherViewController.swift
@@ -142,7 +142,7 @@ extension WeatherViewController {
     }
     
     public override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        viewModel?.placesList.count ?? 0 > 0 ? viewModel?.tableHeaderTitle : ""
+        viewModel?.placesList.count ?? 0 > 0 ? viewModel?.tableHeaderTitle : .none
     }
 }
 
